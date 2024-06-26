07fd2ff3dc50e012d87ff7011ee3046fe240fada
mahfuzswaron <mahfuzswaron@gmail.com>
2024-06-27 01:24:14 +0600

    refactor: cleaned old codes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Home from "./pages/Home";
 function App() {
 
   return (
-    <main className="h-screen w-screen flex items-center justify-center overflow-hidden m-0 px-5 ">
-      <div className="absolute inset-0 ">
+    <main className=" ">
+      <div className="">
         <Home />
       </div>
     </main>
@@ -13,4 +13,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
