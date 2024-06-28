70c0fac4db8c448fc0f49b6b6978eb0853d7bd6c
mahfuzswaron <mahfuzswaron@gmail.com>
2024-06-28 21:04:54 +0600

    refactor: refreshing server done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import Bot from './pages/Bot';
 import "./App.css";
+import { useEffect } from 'react';
 
 
 function App() {
+
+  useEffect(()=>{
+    // refresh server
+    fetch(import.meta.env.VITE_SERVER_URL)
+
+  }, [])
 
   return (
     <main className="w-screen h-screen flex justify-center items-center">
