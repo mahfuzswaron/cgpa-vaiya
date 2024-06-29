407ab5a3e765bdaff568e70f7e286b3d329fbcf6
mahfuzswaron <mahfuzswaron@gmail.com>
2024-06-29 14:25:47 +0600

    feat: hiding address bar automatically done

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,6 @@ import { useEffect } from 'react';
 
 function App() {
 
-  useEffect(()=>{
-    // refresh server
-    fetch(import.meta.env.VITE_SERVER_URL)
-
-  }, [])
-
   return (
     <main className="w-screen h-screen flex justify-center items-center">
       <Bot />
--- src/pages/Bot.jsx
+++ src/pages/Bot.jsx
@@ -3,8 +3,26 @@ import 'react-chatbot-kit/build/main.css'
 import config from '../bot/config';
 import MessageParser from '../bot/MessageParser';
 import ActionProvider from '../bot/ActionProvider';
+import { useEffect } from 'react';
 
 const Bot = () => {
+    useEffect(() => {
+        // Timeout is used to ensure the scroll happens after the initial rendering
+        const hideAddressBar = () => {
+            window.scrollTo(0, 1);
+        };
+
+        // Delay the scroll action to ensure the page is fully loaded
+        setTimeout(hideAddressBar, 100);
+
+        // Add an event listener to re-hide the address bar on orientation change
+        window.addEventListener('orientationchange', hideAddressBar);
+
+        // Cleanup event listener on component unmount
+        return () => {
+            window.removeEventListener('orientationchange', hideAddressBar);
+        };
+    }, []);
     return (
         <div className='container mx-auto w-full '>
             <Chatbot
