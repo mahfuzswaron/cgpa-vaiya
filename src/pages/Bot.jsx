eb1fb7b9ac51968292a74c19edb7730989c740a9
mahfuzswaron <mahfuzswaron@gmail.com>
2024-06-29 16:11:45 +0600

    feat: attemp to make vh dynamic

--- src/pages/Bot.jsx
+++ src/pages/Bot.jsx
@@ -3,27 +3,8 @@ import 'react-chatbot-kit/build/main.css'
 import config from '../bot/config';
 import MessageParser from '../bot/MessageParser';
 import ActionProvider from '../bot/ActionProvider';
-import { useEffect, useState } from 'react';
 
 const Bot = () => {
-    const [isFullscreen, setIsFullscreen] = useState(false);
-
-    useEffect(() => {
-        function onFullscreenChange() {
-            setIsFullscreen(Boolean(document.fullscreenElement));
-        }
-
-        document.addEventListener('fullscreenchange', onFullscreenChange);
-
-        return () => document.removeEventListener('fullscreenchange', onFullscreenChange);
-    }, []);
-
-    useEffect(() => {
-        if (!isFullscreen) {
-            document.body.requestFullscreen().catch(err => console.error(err));
-        }
-    }, [isFullscreen]);
-
     return (
         <div className='container mx-auto w-full '>
             <Chatbot
