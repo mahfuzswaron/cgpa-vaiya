be6e6f8b9f5ce832bb9452bef5789aa23a94a7b7
mahfuzswaron <mahfuzswaron@gmail.com>
2024-06-29 14:14:10 +0600

    refactor: start text updated

--- src/bot/widgets/Start.jsx
+++ src/bot/widgets/Start.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Start = (props) => {
     return (
         <div className='react-chatbot-kit-chat-bot-message-container'>
-            <button className='' onClick={() => props.actions.handleClickStart()}> Let's Start...</button>
+            <button className='' onClick={() => props.actions.handleClickStart()}> Click to Start...</button>
         </div>
     );
 };
